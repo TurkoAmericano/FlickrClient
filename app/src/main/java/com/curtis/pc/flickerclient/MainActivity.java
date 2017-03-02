b4e6b209d0e1a01d087c241b50a1b4f36f2e2a18
TurkoAmericano <cuerhart@yahoo.com>
2017-03-02 10:36:31 +0300

    Lazy loading images into gridView

--- app/src/main/java/com/curtis/pc/flickerclient/MainActivity.java
+++ app/src/main/java/com/curtis/pc/flickerclient/MainActivity.java
@@ -39,13 +39,111 @@ public class MainActivity extends AppCompatActivity {
         try {
             result = task.execute().get();
 
+            JSONObject object = new JSONObject(result).getJSONObject("photos");
+            JSONArray images = object.getJSONArray("photo");
+
+            GridView gridview = (GridView) findViewById(R.id.PhotoGrid);
+
+            gridview.setAdapter(new ImageAdapter(this, images));
+
+            String x = "";
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
 
+    }
+}
+
+class ImageAdapter extends BaseAdapter
+{
+    private Context context;
+    private JSONArray jsonArray;
+
+    public ImageAdapter(Context c, JSONArray j)
+    {
+        context = c;
+        jsonArray = j;
+    }
+
+    //---returns the number of images---
+    public int getCount() {
+        return 20;
+    }
+
+    //---returns the ID of an item---
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    //---returns an ImageView view---
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+
+
+
+        try {
+
+            ImageView imageView = new ImageView(context);
+
+            if (convertView == null)
+            {
+                JSONObject object = jsonArray.getJSONObject(position);
+                String url = object.getString("url_m");
+                String title = object.getString("title");
+
+                new DownloadImageTask(imageView)
+                        .execute(url);
+
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+
+            return imageView;
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return new ImageView(context);
+
+    }
+}
+
+class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
     }
 }
 
--- app/src/main/java/com/curtis/pc/utils/ImageRetrieverTask.java
+++ app/src/main/java/com/curtis/pc/utils/ImageRetrieverTask.java
@@ -16,7 +16,7 @@ public class ImageRetrieverTask  extends AsyncTask<Void, Void, String> {
     protected String doInBackground(Void... params) {
         try {
 
-            URL url = new URL("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=8213afbf41516da43fd1dfa4c36954b0&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1&auth_token=72157680803935026-64774a3dbbf9792b&api_sig=f4addf00b7865aff88de5c840b8663be");
+            URL url = new URL("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=dfbeee85ac373f24739fe537f5d7f4ac&extras=url_m&format=json&nojsoncallback=1&auth_token=72157679053780041-107dac74a9286aa4&api_sig=0b1c6b6f1ccfccbdae0054190539a4e7");
             HttpURLConnection  urlConnection = (HttpURLConnection) url.openConnection();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
